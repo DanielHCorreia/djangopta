83fd23ce7b2505a016e2a836b6383868cdbff608
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-09 16:12:53 -0300

    Conexão com css +  variáveis no html+ admin user

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+#criando entidade Pessoa (feito um tipo)
+class Pessoa(models.Model):
+    name = models.CharField('Nome completo',max_length=100)
+    cpf = models.IntegerField('CPF',primary_key=True)
+    email = models.EmailField('Email',null=True)
+
+    #Usado para ajustar o plural
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Pessoa'
+        verbose_name_plural = 'Pessoas'
+    #Retorna o nome
+    def __str__(self):
+        return self.name
