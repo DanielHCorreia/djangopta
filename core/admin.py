bc14bc0773084a8e1940086ec5fff62520b016ae
Daniel Henrique <dhac@cin.ufpe.br>
2019-11-09 22:17:57 -0300

    add new class Cliente

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Pessoa
+from .models import Cliente
 
 # Register your models here.
 
 admin.site.register(Pessoa)
+admin.site.register(Cliente)
--- core/models.py
+++ core/models.py
@@ -16,3 +16,16 @@ class Pessoa(models.Model):
     #Retorna o nome
     def __str__(self):
         return self.name
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome completo',max_length=100)
+    nomeResumido = models.CharField("Nome resumido",max_length=50)
+    cpf = models.IntegerField('CPF',primary_key=True)
+    dataVencimento = models.DateField('Data de vencimento')
+
+    #class Meta:
+    #   ordering = ['nome']
+    #  verbose_name = 'Cliente'
+    #    verbose_name_plural 'Clientes'
+    def __str__(self):
+        return self.nome
